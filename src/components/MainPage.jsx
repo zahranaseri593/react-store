a26835a822c42c1610ba9ddbe26f00c274faf188
Zahra Naseri <zahranaseri593@gmail.com>
2023-06-05 14:34:18 +0330

    ...

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,7 @@
 import Select from 'react-select';
 import sampleProduct from '../assets/sample.jpg'
+import poster from '../assets/notice.jpg'
+import {BiEnvelope} from 'react-icons/bi'
 
 const sortOpt = [
   { value: 'des', label: 'descending' },
@@ -40,9 +42,29 @@ const MainPage = () => {
                     <div>Men {/*<a href='/' className='product-btn'>add to cart</a>*/}</div>
                     <p>Sale</p>
                 </div>
-                {/* ready to fetch the products from backend and render them to the page
-                
-                */}
+                {/* ready to fetch the products from backend and render them to the page*/}
+            </div>
+
+            <div className='off'>
+              <div className='off-image' style={{backgroundImage: `url(${poster})`}}></div>
+              <div className='off-text'>
+                <p>limited offer</p>
+                <h1>35% off only this friday and get special gift</h1>
+                <a href='#' >Grab it now → </a>
+              </div>
+            </div>
+
+            <div className='newsletter'>
+              <h1>Subscribe to our newsletter to get updates to our latest collections</h1>
+              <p>Get 20% off on your first order just by subscribing to our newsletter</p>
+              <div className='newsletter-input'>
+                <div>
+                <BiEnvelope size='24'/>
+                <input type='text' placeholder='Enter your email'/>
+                </div>
+                <a href="#">Subscribe</a>
+              </div>
+              <p>You will be able to unsubscribe at any time <br/> Read our Privacy Policy <a href='/'>here</a></p>
             </div>
         </div>
      );
